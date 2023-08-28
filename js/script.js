0e2ba7553d70f0ca37f87363618760530164b3b0
Sean S <89683326+SeanSSC@users.noreply.github.com>
2023-08-29 00:35:50 +0800

    Made Navbar, Added some backgrounds, Added javascript to make the navbar "sticky"

--- js/script.js
+++ js/script.js
@@ -1,32 +1,19 @@
 
-var lastScrollPos = window.pageYOffset
-const navBar = document.getElementById("navBar")
-const contact = document.getElementsByClassName("contactMe")[0]
-
+var lastScrollPos = window.scrollY;
+const navBar = document.getElementsByClassName("navBar")[0];
 function scrollAction(){
-    var currentScrollPos = window.pageYOffset
-    navBar.style.top = window.top + "px"
-    if(lastScrollPos > currentScrollPos){
-        //Scroll up        
-        navBar.style.display = "block"
-    }else{
-        //Scroll down
-        navBar.style.display = "none"
-    }
-    lastScrollPos = currentScrollPos
+    var currentScrollPos = window.scrollY
 
+    //Scroll down
+    if (currentScrollPos > lastScrollPos){
+        navBar.style.visibility = "hidden";
+    }
+    //Scroll up
+    else if (currentScrollPos < lastScrollPos){
+        navBar.style.visibility = "visible";
+    }
+    lastScrollPos = currentScrollPos;
 }
 
-function alwaysVisible() {
-    var windowHeight = window.innerHeight;
-    var windowRight = window.innerWidth;
-    var computedStyle = window.getComputedStyle(contact);
-    var contactHeight = parseInt(computedStyle.height);
-    var windowBottomY = windowHeight - (contactHeight);
-    contact.style.top = windowBottomY + "px";
-  }
-  
 
 window.addEventListener('scroll', scrollAction);
-window.addEventListener('scroll', alwaysVisible);
-alwaysVisible;
